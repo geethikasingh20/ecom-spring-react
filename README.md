00219ed0c400c2894772e22e962d39d90fcd9f2b
Geetika <geethika.singh20>
2025-06-30 22:18:35 +0530

    pushing readme file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
+# Full-Stack React + Spring Boot CRUD App + PostgreSQL
+
+A simple ecommerce store built using:
+
+- React (Frontend)
+- Spring Boot (Backend)
+- PostgreSQL Database 
+
+## Features
+
+- Create, Read, Update, Delete (CRUD) tasks
+- RESTful backend API
+- CORS-configured
+- Ready for production deployment
+
+
+## Render.com deployed
+It is deployed on https://ecom-spring-react-1.onrender.com/
+
+## Quick Start
+
+### Backend (Spring Boot)
+```bash
+cd ecom-spring-react
+./mvnw spring-boot:run
 
--- src/main/java/com/example/reactDemo/controller/ProductController.java
+++ src/main/java/com/example/reactDemo/controller/ProductController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +29,40 @@ public class ProductController {
         return productRepository.findAll();
     }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         Product savedProduct = productRepository.save(product);
         return ResponseEntity.ok(savedProduct);
+    }
+    
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product updated) {
+        return productRepository.findById(id)
+                .map(product -> {
+                    product.setName(updated.getName());
+                    product.setPrice(updated.getPrice());
+                    product.setImage(updated.getImage());
+                    Product saved = productRepository.save(product);
+                    return ResponseEntity.ok(saved);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        if (productRepository.existsById(id)) {
+        	productRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }

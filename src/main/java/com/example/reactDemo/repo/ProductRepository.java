cac9ee448359e212bf6d8dcb47fd13600c040603
Geetika <geethika.singh20>
2025-09-01 20:36:42 +0530

    Enabling search by product name

--- src/main/java/com/example/reactDemo/repo/ProductRepository.java
+++ src/main/java/com/example/reactDemo/repo/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.example.reactDemo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.example.reactDemo.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	List<Product> findByNameContainingIgnoreCase(String name);
+
 }

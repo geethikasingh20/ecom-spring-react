52cf357e207b33901dfe38341b0d77ad24e75463
Geetika <geethika.singh20>
2025-10-08 16:37:38 +0530

    new postgresql changes

--- src/main/java/com/example/reactDemo/repo/ProductRepository.java
+++ src/main/java/com/example/reactDemo/repo/ProductRepository.java
@@ -3,11 +3,10 @@ package com.example.reactDemo.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.reactDemo.model.Product;
 
-@Repository
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	List<Product> findByNameContainingIgnoreCase(String name);
